f6a2b7ea467e6294cd3da589bc4eab7ccb3f77b9
mirandanatalia <lya_latina@hotmail.com>
2018-12-14 19:23:47 -0300

    estilos desafios

--- app.js
+++ app.js
@@ -54,16 +54,6 @@ particlesJS.load
   });
 
 
-  //Animaciones
 
-  wow = new WOW({
-    boxClass: 'wow', // default
-    animateClass: 'animated', // default
-    offset: 0, // default
-    mobile: true, // default
-    live: true, // default
-  
-  });
-  wow.init();
   
 
